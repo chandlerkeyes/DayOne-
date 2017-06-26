aba2f64eb18d1b78b963c927369e6d6f23b6184d
chandlerkeyes <ckeyes4@emich.edu>
2017-06-26 16:54:58 -0400

    GC labs

--- src/com/company/Lab1.java
+++ src/com/company/Lab1.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 
 public class Lab1{
     public static void main(String[] args) {
-
+        //create scanner object, declare variables, and create a Lab1Method object
         Lab1Method object = new Lab1Method();
         Scanner keyboard = new Scanner(System.in);
         char input = ' ';
 
-
+        // this loop should iterate if the user enters y, otherwise end of program
         do {
+            //prompt user to input length, width, height(BONUS)
             System.out.println("Enter length:");
             double length = keyboard.nextDouble();
             System.out.println("Enter width:");
@@ -17,10 +18,12 @@ public class Lab1{
             System.out.println("Enter height:");
             double height = keyboard.nextDouble();
 
+            //invoking methods from Lab1Method
             object.area(length, width);
             object.perimeter(length, width);
             object.volume(length, width, height);
 
+            //Ask user if they want to continue
             System.out.println("Continue? (y/n)");
             input = keyboard.next().charAt(0);
         } while (input == 'y');
--- src/com/company/Lab2.java
+++ src/com/company/Lab2.java
@@ -1,7 +1,22 @@
 package com.company;
 
-/**
- * Created by chand on 6/26/2017.
- */
+import java.util.Scanner;
+
 public class Lab2 {
+    public static void main(String[] args) {
+        Lab2Methods object = new Lab2Methods();
+        Scanner keyboard = new Scanner(System.in);
+        int userInput;
+        char yOrN = ' ';
+
+        do {
+            System.out.println("Enter a number between 1 and 100:");
+            userInput = keyboard.nextInt();
+
+            object.evenOdd(userInput);
+
+            System.out.println("Continue? (y/n)");
+            yOrN = keyboard.next().charAt(0);
+        } while (yOrN == 'y');
+    }
 }
